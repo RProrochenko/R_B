507d012348c4fef566b7b17c4f7ae2fa20b2e6dd
Rostyslav Prorochenko <prorochenko91@gmail.com>
2022-07-12 21:47:11 +0300

    Lesson7 fix

--- Lesson7/status2.sh
+++ Lesson7/status2.sh
@@ -6,8 +6,9 @@ process_pid=$(pidof $name)
 for PID in $process_pid
     do
         sudo kill -15 $PID
-        #sleep 10
+        sleep 10
         check=$(pidof $name)
+        
         if  pidof $check | grep $PID
             then
             sudo kill -9 $PID
