d9105e7055c713b0b4cbcba36c23a18c19a8030e
Rostyslav Prorochenko <prorochenko91@gmail.com>
2022-07-27 14:14:38 +0300

    Lesson10

---
+++ Lesson10/route_delete.sh
@@ -0,0 +1,4 @@
+sudo ip route del 212.100.54.0/26 via 172.26.51.0
+sudo ip route del 212.100.54.64/26 via 172.26.51.0
+sudo ip route del 212.100.54.128/26 via 172.26.51.0
+sudo ip route del 212.100.54.192/26 via 172.26.51.0
